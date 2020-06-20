9db96596fc91fd4f3a70bdbf418a3f58fb75abf0
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-06-20 17:50:25 +0900

    [FEATURE](Webpack): setup webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: './src/app.ts',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
   },
   devtool: 'inline-source-map',
   module: {
