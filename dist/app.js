8e33009f26100c529bce6bd7b399b83bb355e3db
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-06-20 17:58:48 +0900

    [FEATURE](Webpack): npm run build

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-import { ProjectInput } from "./components/projectInput.js";
-import { ProjectList } from "./components/projectList.js";
-new ProjectInput();
-new ProjectList("active");
-new ProjectList("finished");
-//# sourceMappingURL=app.js.map
--- dist/models/dragDrop.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=dragDrop.js.map
